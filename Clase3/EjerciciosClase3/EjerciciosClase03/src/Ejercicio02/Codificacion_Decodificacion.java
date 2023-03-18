6d9e88360e41616afd3c66b78823d5bd0229f6a3
luciaSor <sorialuciasofia@gmail.com>
2023-03-18 02:44:48 -0300

    onceavo-commit

--- Clase3/EjerciciosClase3/EjerciciosClase03/src/Ejercicio02/Codificacion_Decodificacion.java
+++ Clase3/EjerciciosClase3/EjerciciosClase03/src/Ejercicio02/Codificacion_Decodificacion.java
@@ -2,22 +2,32 @@ package Ejercicio02;
 
 public class Codificacion_Decodificacion {
 	
-public static void Metodo_codificacion(int argumento1){
+public static void Metodo_codificacion(){
 		
+	int desplazamiento = 1 ;
 		char[] abecedario = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','ñ','o','p','q','r','s','t','u','v','w','x','y','z'};
-
-		char[] chars = "hola que tal z".toLowerCase().toCharArray();
+		String mensaje = "hola que tal z";
+		char[] texto = mensaje.toCharArray();
+	
 		
-	// Intente varias formas de lograr este ejercicio pero no lo logré
-		// parece que no lo entendi muy bien
+        for (int i = 0; i < texto.length; i++) {
+        	for(int j=0;j<abecedario.length;j++) {
+        		if(texto[i] == abecedario[j]) {
+        			int auxiliar = j + desplazamiento;
+        			//texto[i] = abecedario[auxiliar];
+        		}
+            }
+        }
+        
+        	System.out.println(texto);
+        
+        return;
 }
 
 	
 	public static void main(String[] args) {
 		
-	
-		int desplazamiento = 1 ;
-		
-		Metodo_codificacion( desplazamiento);
+		  Metodo_codificacion();
 	}
 }
+
